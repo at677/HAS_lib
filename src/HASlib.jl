5e1645d4356c949c5a21ed9708ff6b9c479f1de0
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2018-09-27 10:55:21 +0200

    add tests

--- src/HASlib.jl
+++ src/HASlib.jl
@@ -1,4 +1,5 @@
 module HASlib
+export hexgrid, h2m, cc
 function hexgrid(n::Int64)
     a = [1.0 -0.5;
          0.0 sqrt(3.0)/2.0]
@@ -27,4 +28,40 @@ function hexgrid(n::Int64)
     end
     return g,gi
 end
+
+function h2m(m)
+    hb = 1.054571800e-34
+    hb^2/(2*m)
+end
+
+function cc(Ei,theta,m,maxgrid=5,st_p_wl=100)
+
+    Ei = Ei*1.6e-19
+
+    ki = sqrt(Ei/h2m(m))
+    Ki = ki*sin(theta*180/π)
+
+    g,gi = hexgrid(maxgrid)
+    g = g.*1e10
+
+    kz2 = -((g[:,1].+Ki).^2 .+ g[:,2].^2).+ ki^2
+
+    index = sortperm(kz2)
+    sorted_kz2 = kz2[index]
+    
+    #find lowest open channel
+    min_index_open = argmax(sorted_kz2 .>= 0.0)
+    println(sorted_kz2[min_index_open])
+    println(min_index_open)
+    println(sorted_kz2[min_index_open-1])
+
+    e_max = maximum(kz2)
+    step = 2*π / (st_p_wl*sqrt(e_max))
+
+    #coupling
+
+
+
+end
+
 end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Test
+using HASlib
+
+@test size(HASlib.hexgrid(1)[1])==(7,2)
+@test isapprox(h2m(1), 5.56e-69;atol=1e-71,rtol=0)
+println( HASlib.cc(3,2,3*1.6e-27,4))

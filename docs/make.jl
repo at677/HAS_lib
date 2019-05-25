ce157cc8dfd7a4bf2d4074193c4219c6d2e40021
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2019-05-25 15:15:38 +0200

    deploy docs to github

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,7 @@ makedocs(
     format = Documenter.HTML(prettyurls = false)
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/feanor12/HASlib.jl.git",
+)
+
